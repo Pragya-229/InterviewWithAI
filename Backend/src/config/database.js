3c11c5a028f05d781f6221df7b47dde8a5c5550b
Pragya Dubey <166438867+Pragya-229@users.noreply.github.com>
2026-03-27 11:10:27 +0530

    first commit

---
+++ Backend/src/config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+
+
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
+
+async function connectToDB() {
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+
+        console.log("Connected to Database")
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+module.exports = connectToDB
