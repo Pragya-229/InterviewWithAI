199309acc4f9093f12d19705eff9f212f73111e5
Pragya Dubey <166438867+Pragya-229@users.noreply.github.com>
2026-03-27 12:03:27 +0530

    Fix error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://interviewwithai-ynx9.onrender.com",
     credentials: true
 }))
 
