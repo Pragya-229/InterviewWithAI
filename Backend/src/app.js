64b760927a818992a129f4148365391c0731019d
Pragya Dubey <166438867+Pragya-229@users.noreply.github.com>
2026-04-07 16:26:35 +0530

    fix auth

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,6 +7,7 @@ const app = express()
 const defaultAllowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://interview-with-ai-theta.vercel.app",
     "https://interviewai-brn4.onrender.com"
 ]
 
